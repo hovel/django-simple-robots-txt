46656b42b9340e4cacfa509f627d8b0ebf9ee0a6
Pavel Zhukov <gelios@gmail.com>
2020-08-18 02:40:31 +0300

    Django 3

--- simple_robots_txt/models.py
+++ simple_robots_txt/models.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from solo.models import SingletonModel
 
 
-@python_2_unicode_compatible
+
 class RobotsTXT(SingletonModel):
     content = models.TextField()
 
